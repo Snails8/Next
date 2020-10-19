0c2756898590838d901f23fe44f46ae03c31b7a8
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-19 14:25:33 +0900

    add:table設計

--- database/migrations/2020_10_19_020118_create_tasks_table.php
+++ database/migrations/2020_10_19_020118_create_tasks_table.php
@@ -14,7 +14,9 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->integer('status');
             $table->timestamps();
         });
     }
