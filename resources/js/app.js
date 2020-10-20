9754ae72e5cd0dbcf205ca37970d68683d4c9920
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-20 16:59:02 +0900

    add:必要なパッケージをインストール

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,21 @@ import './bootstrap'
 import Vue from 'vue'
 import Sample from './components/Sample'
 
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
 
 Vue.component('sample-component',require('./components/Sample.vue').default);
+Vue.component('calender-component',require('./components/CalenderComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
     components: {
         Sample
     }
+    Vuetify: new Vuetify({
+
+    }),
 });
