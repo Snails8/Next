f6717b35b4c688611e9d6bfa7813097531c9a4a1
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-21 16:42:08 +0900

    add:calendar作成　大枠のクラスを作成

---
+++ app/Calendar/CalendarView.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Calendar;
+
+use Carbon\Carbon;
+
+class CalendarView 
+{
+    private $carbon;
+
+    public function __construct($date)
+    {
+        // コンストラクタで受け取った日付を元に
+        // Carbonオブジェクトを作成
+        $this->carbon = new Carbon($date);
+
+    }
+
+    // タイトル
+    public function getTitle()
+    {
+        return $this->carbon->format('Y年n月');
+    }
+
+    // カレンダーを出力する
+    public function render()
+    {
+        $html = [];
+        $html = '<table class="table">';
+        $html[] = '<thead>';
+		$html[] = '<tr>';
+		$html[] = '<th>月</th>';
+		$html[] = '<th>火</th>';
+		$html[] = '<th>水</th>';
+		$html[] = '<th>木</th>';
+		$html[] = '<th>金</th>';
+		$html[] = '<th>土</th>';
+        $html[] = '<th>日</th>';
+		$html[] = '</tr>';
+		$html[] = '</thead>';
+		$html[] = '</table>';
+		$html[] = '</div>';
+		return implode("", $html);
+    }
+    
+}
