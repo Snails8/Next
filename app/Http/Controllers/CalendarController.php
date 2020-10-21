5a5455bde91f967c3ef904b2d7ef4978602c0633
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-21 16:33:34 +0900

    add:calendar作成　controller

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Facade;
+use App\Calender\Calender;
 
 class CalendarController extends Controller
 {
-    //
+
+    public function index()
+    {
+        
+        
+    }
 }
