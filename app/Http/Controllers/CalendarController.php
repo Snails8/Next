981341424eb2ceb1570a6ba1be3c979b170789ba
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-21 16:49:24 +0900

    add:calendar コントローラー追加

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Calendar\CalendarView;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Facade;
-use App\Calender\Calender;
+use App\Calender\CalenderView;
 
 class CalendarController extends Controller
 {
-
-    public function index()
+    public function show()
     {
-        
-        
+        //time()で現在時刻を渡し今月のカレンダーを用意
+        $calendar = new CalendarView(time());
+
+        //Viewに作成したCalendarViewオブジェクトを渡す
+        return view('calendar', [
+            "calendar" => $calendar
+        ]);
     }
 }
