d025fbc57a9d8c768f9b2ecc0d4a0e437a72f21f
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-22 11:54:49 +0900

    add:calendar作成　週クラス作成

---
+++ app/Calendar/CalendarWeek.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Calendar;
+
+use Carbon\Carbon;
+
+class CalendarWeek 
+{
+
+    protected $carbon;
+    protected $index = 0;
+
+    public function __construct($date, $index = 0)
+    {
+        $this->carbon = new Carbon($date);
+        $this->index = $index;
+    }
+
+    public function getClassname()
+    {
+        return "week-" . $this->index;
+    }
+    /**
+     * @return CalendarWeekDay[]
+     */
+
+    public function getDays()
+    {
+        $days = [];
+
+        //Carbonを利用して、開始日〜終了日の作成
+        $startDay = $this->carbon->copy()->startOfWeek();
+        $lastDay = $this->carbon->copy()->endOfWeek;
+
+        //作業用
+        $tmpDay = $startDay->copy();
+
+        //月〜日までループ
+        while($tmpDay->lte($lastDay))
+        {
+            //前の月or後ろの月の場合空白を表示。
+            //月を比較して違う場合は前or後ろの余白なので処理を分ける
+            if($tmpDay->month != $this->carbon->month)
+            {
+                $day = new CalendarWeekBlankDay($tmpday->copy());
+                $days[] =$day;
+                $tmpDay->addDay(1);
+                continue;
+            }
+
+            //今月
+            $day = new CalendarWeekDay($tmpDay->copy());
+            $days[] = $day;
+            //翌月に移動
+            $tmpDay->addDay(1);
+        }
+
+        return $days;
+
+    }
+
+}
