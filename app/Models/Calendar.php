1bf95b532b69434cd7cc14c95c5a4fe6e35d5e75
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-23 17:08:44 +0900

    add:タスク追加機能　状態データの作成

---
+++ app/Models/Calendar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Calendar extends Model
+{
+    use HasFactory;
+
+    const STATUS = [
+        1 => [ 'label' => '悪い', 'class' => 'label-danger' ],
+        2 => [ 'label' => '普通', 'class' => 'label-info' ],
+        3 => [ 'lable' => '良い', 'class' => '' ],
+    ]
+}
