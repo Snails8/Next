4a26b2811c92479b0222daa193407e7b4aaeafe7
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-03 18:44:35 +0900

    fix:model Taskモデルに統一

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $table = "tasks";
+
+    protected $fillable = [
+        'title',
+        'status',
+        'description',
+        
+    ];
+
+    const STATUS = [
+        1 => [ 'label' => '悪い', 'class' => 'label-danger' ],
+        2 => [ 'label' => '普通', 'class' => 'label-info' ],
+        3 => [ 'lable' => '良い', 'class' => '' ],
+    ];
 }
