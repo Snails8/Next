5f1c94426acb9955eda1b03eb61fd4b4b96368e3
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-10 14:40:26 +0900

    add:リファクタリング

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,12 +8,7 @@ use App\Models\Task;
 use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
-{
-    public function index()
-    {
-        return view('layout');
-    }
-    
+{   
     public function create(CreateTask $request)
     {
         $task = new Task();
@@ -37,39 +32,5 @@ class TaskController extends Controller
         $calendar = Task::find($id);
 
         return view('tasks.show', compact('carendar'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
