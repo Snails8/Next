89ab5747f7908af0b54b54ac9a9aa8725a26d914
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-12 13:18:02 +0900

    add:詳細表示機能　blade

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,5 +1,27 @@
-@extends('layout');
+@extends('layout')
 
-@section('content');
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <div class="card">
+          <div class="card-header">
+              詳細
+          </div>
 
+          <div class="card-body">
+            @foreach($tasks as $task)
+              <tr>
+                  <td>{{ $task->title }}</td>
+                  <td>
+                    <span class="label">{{ $task->$status }}</span>
+                  </td>
+                  <td>{{ $task->description }}</td>
+              </tr>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
