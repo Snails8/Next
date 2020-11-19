445110f83dca88f58a5b345a237ca64fbbd3e0fa
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-19 11:11:24 +0900

    improve:Day リファクタリング

--- app/Calendar/CalendarWeekday.php
+++ app/Calendar/CalendarWeekday.php
@@ -37,10 +37,11 @@ class CalendarWeekDay{
         $html[] = '<p class="day">' . $this->carbon->format("j"). '</p>';
         $html[] = '<a href="/next/tasks/1">ココ！</a>';
 
-        $active_date = Task::where("active_date");
+        $active_date = Task::where("active_date", "=", $date);
 
         if($date === $active_date)
-        {              
+        {
+
             $html[] = '<a href="next/tasks/{id}"></a>';
         }else
         {
