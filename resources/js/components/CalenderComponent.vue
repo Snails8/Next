c8d76156d0d70cfab8bfe65bb2f9797c094076e2
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-21 19:37:12 +0900

    add:calendar componentの作成

--- resources/js/components/CalenderComponent.vue
+++ resources/js/components/CalenderComponent.vue
@@ -13,3 +13,34 @@
         </v-sheet>
     </div>
 </template>
+
+<script>
+export default {
+  date: () => ({
+      today: '2020-11-20',
+      type: 'month',
+      datas:[],
+      events: [
+          {
+              name: 'タスクある',
+              start: '2020-11-19',
+              end: '2020-11-01',
+          },
+          {
+              name: 'Meeting',
+              start: '2020-11-1 09:00',
+              end: '2020-05-1 10:00',
+          },
+      ],
+  }),
+  computed:{
+
+  },
+  mounted () {
+
+  },
+  methods:{
+
+  },
+}
+</script>
