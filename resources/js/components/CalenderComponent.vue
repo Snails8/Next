f68a7d90ef97bccc1383a079f94b0e629cea3074
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-21 19:09:54 +0900

    add:vue template追加

---
+++ resources/js/components/CalenderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-sheet height="500">
+            <v-calendar
+              ref="calendar"
+              locale="ja"
+              :type="type"
+              :now="today"
+              :value="today"
+              :events="events"
+              color="primary">
+            </v-calendar>
+        </v-sheet>
+    </div>
+</template>
