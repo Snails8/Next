84112e0c7ce339e4120bc945c54ec22812acd80a
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-14 17:32:36 +0900

    add:sample-component vue連携

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,24 @@
     <body>
        
         <div id="app">
-          <v-app>
             <sample-component></sample-component>
-        </v-app>
+        </div>
+
+        <div>
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
         </div>
 
     <script src="{{ mix('/js/app.js') }}"></script>
