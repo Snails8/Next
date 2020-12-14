f476281c6df218de3aa70f1db7f7c78a1f909148
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-14 17:22:47 +0900

    add:SPA化 router追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import './bootstrap'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import Sample from './components/Sample'
 
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-
 
 Vue.component('sample-component',require('./components/Sample.vue').default);
 Vue.component('calender-component',require('./components/CalenderComponent.vue').default);
