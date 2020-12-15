3259b4d33a9fcf1df02c0d0c184b63187a19ec17
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-15 16:42:25 +0900

    add:pathのマッピング

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from './pages/Login.vue'
+
 Vue.use(vueRouter)
 
 const routes = [
-    
+    {
+        path: '/login',
+        component: Login   
+    }
 ]
+
+const router = new vueRouter({
+    routes
+})
 
 export default router 
