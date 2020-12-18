cceca52646a060b0df9ac503ca968476d85931a1
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-18 16:58:44 +0900

    add:ルーティング

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskCreateController;
 use App\Http\Controllers\CalendarController;
-use App\Http\Controllers\AppController;
 
-Route::get('/', [AppController::class, 'showApi']);
+Route::get('/', function(){
+    return view('welcome');
+});
 
 /**
  * カレンダー一覧表示
