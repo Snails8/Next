558d0dc0fadf34434cc6e2b97a7f207c0d71dcf0
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-19 19:30:22 +0900

    add:カレンダー　vuetifyの追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,21 @@ import './bootstrap'
 import Vue from 'vue'
 import Sample from './components/Sample'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.js'
+import vuetifyMin from 'vuetify/dist/vuetify.min.js'
+
+Vue.use(Vuetify)
+
 Vue.component('sample-component',require('./components/Sample.vue').default);
+Vue.component('calendar-component', required('./components/CalendarComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
     components: {
         Sample
-    }
+    },
+    vuetify: new Vuetify({
+
+    }),
 });
