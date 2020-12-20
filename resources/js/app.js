6eebcc59e71c783a8b03d14b5e8a8ad819f7ff92
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-20 20:12:26 +0900

    add:calendar v-calendarの読み込み

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,24 @@
 import './bootstrap'
 import Vue from 'vue'
 import Sample from './components/Sample'
+import App from './App.vue'
 
+import VCalendar from 'v-calendar'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.js'
 import vuetifyMin from 'vuetify/dist/vuetify.min.js'
 
 Vue.use(Vuetify)
+Vue.use(VCalendar)
+
+Vue.config.productionTip = false
 
 Vue.component('sample-component',require('./components/Sample.vue').default);
 Vue.component('calendar-component', required('./components/CalendarComponent.vue').default);
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
 
 const app = new Vue({
     el: '#app',
