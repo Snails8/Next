522060de8484ef2a66f3b4be08b5a8d5a10ee169
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-20 20:08:48 +0900

    add:calendar calendar.vueの呼び出し処理

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,28 @@
 <template>
-    <div>
-        <main>
-            <div class="container">
-                <RouteView />
-            </div>
-        </main>
+    <div id="app">
+        <calendar/>
     </div>
 </template>
+
+<script>
+import calendar from './components/Calendar.vue'
+
+export default {
+    name: 'app',
+    components: {
+        calendar
+    }
+    
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
