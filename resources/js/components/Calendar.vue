a0139c4c926617e0d1142793e15a2939f648968c
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-20 21:21:57 +0900

    add:calendar html部分

---
+++ resources/js/components/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-calendar
+        :columns="2"
+        title-position="center"
+        >
+        <template slot='header-title' slot-scope='props'>
+            {{props.yearLabel}}年{{props.monthLabel}}
+        </template>
+
+        <template slot='day-content' slot-scope='props'>
+            <div class="cell-header">
+                {{props.day.day}}
+            </div>
+            <div class="cell-contemt">
+            <template
+              v-if="dataList.some(date => data.ymd === dateToYYYYMMDD(props.day.date))">
+                <div
+                class="cell-content-line"
+                v-for="content in getContentFromKey(dateToYYYYMMDD(props.day.date))"
+                v-bind:key="content">
+                  ・{{content}}
+                </div>
+            </template>
+            </div>
+        </template>
+
+        </v-calendar>
+    </div>
+
+</template>
